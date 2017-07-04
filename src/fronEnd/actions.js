57cfaf14e31af528e9bf542a68d56e1f1e7ad4b2
2sad <2sad@localhost.localdomain>
2017-07-04 23:23:29 +0300

    fix xmlhttprequest

--- src/fronEnd/actions.js
+++ src/fronEnd/actions.js
@@ -1,12 +1,12 @@
 const getApiData = () => {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
+    if (this.readyState == 4 && this.status == 200) {
+      // Typical action to be performed when the document is ready:
       console.log(xhttp.responseText);
-      }
+    }
   };
-  xhttp.open("GET", ' http://thegamesdb.net/api/GetArt.php?id=2', true);
-  xhttp.setRequestHeader("Access-Control-Allow-Origin","http://thegamesdb.net/");
+  xhttp.open("GET", " http://thegamesdb.net/api/GetArt.php?id=2", true);
   xhttp.send();
 }
 export {

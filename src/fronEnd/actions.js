2cd5ff820fd7a8a5dd28a53847aaf019de60bf1e
2sad <2sad@localhost.localdomain>
2017-07-05 11:19:22 +0300

    get pictures

--- src/fronEnd/actions.js
+++ src/fronEnd/actions.js
@@ -1,9 +1,15 @@
+const xml2js = require('xml2js');
+const parser = new xml2js.Parser();
 const getApiData = () => {
-  var xhttp = new XMLHttpRequest();
+  const xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      // Typical action to be performed when the document is ready:
-      console.log(xhttp.responseText);
+      const xml = xhttp.responseText;
+      parser.parseString(xml, (err, result) => {
+        const url = result.Data.baseImgUrl[0];
+        const images = result.Data.Images[0].fanart.map((data) => url + data.original[0]._);
+        console.log(images);
+      });
     }
   };
   xhttp.open("GET", " http://thegamesdb.net/api/GetArt.php?id=2", true);
